ab3e14698d11eafe58e53b52bc2f7e294c5d0a1a
amishademo <150772818+amishademo@users.noreply.github.com>
2023-11-14 11:35:30 +0530

    Add files via upload

---
+++ add_two_no.java
@@ -0,0 +1,59 @@
+package Operators;
+
+//WAP to accept two no and find out the addition
+import java.util.Scanner;
+
+public class add_two_no 
+{
+	public static void main(String[] args) 
+	{
+		/*
+	
+		int num1,num2,res;
+		//Scanner class to accept the values 
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Arithmetic Operator.");
+		System.out.println("Addition:");
+		
+		System.out.println("Enter two numbers");
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		//Calculation for addition
+		res=num1+num2;
+
+		System.out.println("The addition is ="+res);
+		
+		
+		System.out.println(200+22+" is the result of addition and \nnew no addd is ="+67+88);
+		
+		System.out.println("addition is="+ 10+20);
+		System.out.println( 10+20+"addition is=");
+		
+		
+		int num=10;
+		int res;
+		res=num--;
+		
+		System.out.println(res);
+		System.out.println(num);
+	
+		int num2=89;
+		num2+=2;
+		System.out.println("num="+num2);
+		
+		*/
+		
+		int a=10,b=10;
+		boolean res;
+		
+		res=!((a<b)||(a>b));
+		
+		System.out.println("res="+res);
+		
+		
+		
+		
+	}
+
+}
